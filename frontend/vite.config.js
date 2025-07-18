d9b1d4378dd89bc0574a5ff8cf601d3aa43919d9
Harshit Raj <rajharshit789@gmail.com>
2025-07-18 16:48:59 +0530

    Fix Vite configuration and add Vercel rewrites for serving index.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-<<<<<<< HEAD
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),],
-=======
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()
-    ,tailwindcss(),
-  ],
->>>>>>> 3b5598e8cf853d3331594446a484fe17bcec96de
 })
